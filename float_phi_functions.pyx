76220afba221529b23ae83b6050da82a084ef496
Fredriclam <fredriclam@live.com>
2023-03-26 14:26:33 -0700

    Rework Cython coefficient memory locality, remove extra ops

--- float_phi_functions.pyx
+++ float_phi_functions.pyx
@@ -52,11 +52,65 @@ cdef DTYPE_t[56] t_res = [
   44.   , 46.   , 50.   ,  0.   ,  1.   ,  4.   ,  0.   ,  0.   ]
 cdef DTYPE_t[2] _exp1_55_56 = [1.66666667, 1.66666667]
 
-@cython.boundscheck(False)
-@cython.wraparound(False)
-@cython.nonecheck(False)
-@cython.cdivision(True)
-@cython.profile(True)
+# Fused coefficient array for 1 to 51.
+#   Coefficients (n, d, t, c) are contiguous in memory
+cdef DTYPE_t[204] ndtc1_51 = [
+        1.25335479e-02,  1.00000000e+00, -5.00000000e-01,  0.00000000e+00,
+        7.89576347e+00,  1.00000000e+00,  8.75000000e-01,  0.00000000e+00,
+       -8.78032033e+00,  1.00000000e+00,  1.00000000e+00,  0.00000000e+00,
+        3.18025093e-01,  2.00000000e+00,  5.00000000e-01,  0.00000000e+00,
+       -2.61455339e-01,  2.00000000e+00,  7.50000000e-01,  0.00000000e+00,
+       -7.81997517e-03,  3.00000000e+00,  3.75000000e-01,  0.00000000e+00,
+        8.80894931e-03,  4.00000000e+00,  1.00000000e+00,  0.00000000e+00,
+       -6.68565723e-01,  1.00000000e+00,  4.00000000e+00,  1.00000000e+00,
+        2.04338110e-01,  1.00000000e+00,  6.00000000e+00,  1.00000000e+00,
+       -6.62126050e-05,  1.00000000e+00,  1.20000000e+01,  1.00000000e+00,
+       -1.92327212e-01,  2.00000000e+00,  1.00000000e+00,  1.00000000e+00,
+       -2.57090430e-01,  2.00000000e+00,  5.00000000e+00,  1.00000000e+00,
+        1.60748685e-01,  3.00000000e+00,  4.00000000e+00,  1.00000000e+00,
+       -4.00928289e-02,  4.00000000e+00,  2.00000000e+00,  1.00000000e+00,
+        3.93434226e-07,  4.00000000e+00,  1.30000000e+01,  1.00000000e+00,
+       -7.59413771e-06,  5.00000000e+00,  9.00000000e+00,  1.00000000e+00,
+        5.62509794e-04,  7.00000000e+00,  3.00000000e+00,  1.00000000e+00,
+       -1.56086523e-05,  9.00000000e+00,  4.00000000e+00,  1.00000000e+00,
+        1.15379964e-09,  1.00000000e+01,  1.10000000e+01,  1.00000000e+00,
+        3.65821651e-07,  1.10000000e+01,  4.00000000e+00,  1.00000000e+00,
+       -1.32511801e-12,  1.30000000e+01,  1.30000000e+01,  1.00000000e+00,
+       -6.26395869e-10,  1.50000000e+01,  1.00000000e+00,  1.00000000e+00,
+       -1.07936009e-01,  1.00000000e+00,  7.00000000e+00,  2.00000000e+00,
+        1.76114910e-02,  2.00000000e+00,  1.00000000e+00,  2.00000000e+00,
+        2.21322952e-01,  2.00000000e+00,  9.00000000e+00,  2.00000000e+00,
+       -4.02476698e-01,  2.00000000e+00,  1.00000000e+01,  2.00000000e+00,
+        5.80834000e-01,  3.00000000e+00,  1.00000000e+01,  2.00000000e+00,
+        4.99691470e-03,  4.00000000e+00,  3.00000000e+00,  2.00000000e+00,
+       -3.13587007e-02,  4.00000000e+00,  7.00000000e+00,  2.00000000e+00,
+       -7.43159297e-01,  4.00000000e+00,  1.00000000e+01,  2.00000000e+00,
+        4.78073299e-01,  5.00000000e+00,  1.00000000e+01,  2.00000000e+00,
+        2.05279409e-02,  6.00000000e+00,  6.00000000e+00,  2.00000000e+00,
+       -1.36364351e-01,  6.00000000e+00,  1.00000000e+01,  2.00000000e+00,
+        1.41806344e-02,  7.00000000e+00,  1.00000000e+01,  2.00000000e+00,
+        8.33265049e-03,  9.00000000e+00,  1.00000000e+00,  2.00000000e+00,
+       -2.90523360e-02,  9.00000000e+00,  2.00000000e+00,  2.00000000e+00,
+        3.86150856e-02,  9.00000000e+00,  3.00000000e+00,  2.00000000e+00,
+       -2.03934865e-02,  9.00000000e+00,  4.00000000e+00,  2.00000000e+00,
+       -1.65540501e-03,  9.00000000e+00,  8.00000000e+00,  2.00000000e+00,
+        1.99555720e-03,  1.00000000e+01,  6.00000000e+00,  2.00000000e+00,
+        1.58703083e-04,  1.00000000e+01,  9.00000000e+00,  2.00000000e+00,
+       -1.63885683e-05,  1.20000000e+01,  8.00000000e+00,  2.00000000e+00,
+        4.36136157e-02,  3.00000000e+00,  1.60000000e+01,  3.00000000e+00,
+        3.49940055e-02,  4.00000000e+00,  2.20000000e+01,  3.00000000e+00,
+       -7.67881978e-02,  4.00000000e+00,  2.30000000e+01,  3.00000000e+00,
+        2.24462773e-02,  5.00000000e+00,  2.30000000e+01,  3.00000000e+00,
+       -6.26897104e-05,  1.40000000e+01,  1.00000000e+01,  4.00000000e+00,
+       -5.57111186e-10,  3.00000000e+00,  5.00000000e+01,  6.00000000e+00,
+       -1.99057184e-01,  6.00000000e+00,  4.40000000e+01,  6.00000000e+00,
+        3.17774973e-01,  6.00000000e+00,  4.60000000e+01,  6.00000000e+00,
+       -1.18411824e-01,  6.00000000e+00,  5.00000000e+01,  6.00000000e+00]
+
+# @cython.boundscheck(False)
+# @cython.wraparound(False)
+# @cython.nonecheck(False)
+# @cython.cdivision(True)
 def phir_d(DTYPE_t d, DTYPE_t t):
   ''' First delta-derivative of residual part of dimless Helmholtz function
       phi = f/(RT).
@@ -64,55 +118,62 @@ def phir_d(DTYPE_t d, DTYPE_t t):
   Cython implementation for float input.
   '''
 
-  # Precompute quantities
   cdef DTYPE_t d_quad = (d - 1.0) * (d - 1.0)
-  # Allocate and partially evaluate coeffs
-  cdef DTYPE_t coeffs[56]
-  cdef DTYPE_t theta
-  cdef DTYPE_t Delta
+  cdef DTYPE_t _theta
+  cdef DTYPE_t _Delta
+  cdef DTYPE_t _neg_dpowc
+  cdef DTYPE_t out = 0.0
+
+  # Use strides as below
+  # cdef n_coeff ndtc1_51[4*i]
+  # cdef d_coeff ndtc1_51[4*i+1]
+  # cdef t_coeff ndtc1_51[4*i+2]
+  # cdef c_coeff ndtc1_51[4*i+3]
+
+  # Compute uniform coefficients with mixed data (1-indices 1 to 51)
   cdef unsigned int i
+  for i in range(7):
+    # out += n_coeff * d_coeff * (d ** (d_coeff-1.0)) * (t ** t_coeff)
+    out += ndtc1_51[4*i] \
+      * ndtc1_51[4*i+1] \
+      * (d ** (ndtc1_51[4*i+1]-1.0)) * (t ** ndtc1_51[4*i+2])
+  for i in range(7, 51):
+    _neg_dpowc = -d ** ndtc1_51[4*i+3]
+    out += ndtc1_51[4*i] \
+      * (ndtc1_51[4*i+1] + ndtc1_51[4*i+3] * _neg_dpowc) \
+      * (d ** (ndtc1_51[4*i+1]-1.0)) * (t ** ndtc1_51[4*i+2]) \
+      * exp(_neg_dpowc)
+
   # Compute pre-exponential coefficients
-  for i in range(56):
-    coeffs[i] = n_res[i] * (d ** (d_res[i]-1.0)) * (t ** t_res[i])
-  for i in range(51):
-    coeffs[i] *= (d_res[i] - c_res1_51[i] * d ** c_res1_51[i])
+  cdef DTYPE_t _d_shift
+  cdef DTYPE_t _t_shift
+  cdef DTYPE_t _coeff  
+  # Compute heterogeneous coefficients for 1-indices 52 to 54
   for i in range(51,54):
-    coeffs[i] *= d_res[i] - 2.0 * alpha_res52_54[i-51] * d * (d - eps_res52_54[i-51])
+    _coeff = n_res[i] * (d ** (d_res[i]-1.0)) * (t ** t_res[i]) \
+      * (d_res[i] - 2.0 * alpha_res52_54[i-51] * d * (d - eps_res52_54[i-51]))
+    _d_shift = d - eps_res52_54[i-51]
+    _t_shift = t - gamma_res52_54[i-51]
+    out += _coeff * exp(-alpha_res52_54[i-51] * _d_shift * _d_shift \
+      -beta_res52_54[i-51] * _t_shift * _t_shift)
+  # Compute heterogeneous coefficients for 1-indices 55 to 56
   for i in range(54,56):
-    theta = (1.0 - t) + A_res55_56[i-54] * d_quad ** _exp1_55_56[i-54]
-    Delta = theta*theta + B_res55_56[i-54] * d_quad ** a_res55_56[i-54]
-    coeffs[i] *= (
-      Delta * (1.0 - 2.0 * C_res55_56[i-54] * (d-1.0) * d)
+    _coeff = n_res[i] * (d ** (d_res[i]-1.0)) * (t ** t_res[i])
+    _theta = (1.0 - t) + A_res55_56[i-54] * d_quad ** _exp1_55_56[i-54]
+    _Delta = _theta*_theta + B_res55_56[i-54] * d_quad ** a_res55_56[i-54]
+    _coeff *= (
+      _Delta * (1.0 - 2.0 * C_res55_56[i-54] * (d-1.0) * d)
       + b_res55_56[i-54] * d * (d-1.0) * (
-        A_res55_56[i-54] * theta * 2.0 / beta_res55_56[i-54]
+        A_res55_56[i-54] * _theta * 2.0 / beta_res55_56[i-54]
         * d_quad**(_exp1_55_56[i-54] - 1.0)
         + 2.0 * B_res55_56[i-54] * a_res55_56[i-54]
         * d_quad**(a_res55_56[i-54] - 1.0)
       )
     )
-    if Delta != 0:
-      Delta = Delta ** (b_res55_56[i-54]-1.0)
-    coeffs[i] *= Delta
+    if _Delta != 0:
+      _Delta = _Delta ** (b_res55_56[i-54]-1.0)
+    _coeff *= _Delta
+    out += _coeff * exp(-C_res55_56[i-54] * d_quad \
+      - D_res55_56[i-54]*(t - 1.0)*(t - 1.0))
 
-  # Allocate exponent cache
-  cdef DTYPE_t exponents[56]
-  cdef DTYPE_t _d_shift
-  cdef DTYPE_t _t_shift
-  # Compute exponents
-  for i in range(7):
-    exponents[i] = 0.0
-  for i in range(7,51):
-    exponents[i] = -d ** c_res1_51[i]
-  for i in range(51,54):
-    _d_shift = d - eps_res52_54[i-51]
-    _t_shift = t - gamma_res52_54[i-51]
-    exponents[i] = -alpha_res52_54[i-51] * _d_shift * _d_shift \
-      -beta_res52_54[i-51] * _t_shift * _t_shift
-  for i in range(54,56):
-    exponents[i] = -C_res55_56[i-54] * d_quad \
-      - D_res55_56[i-54]*(t - 1.0)*(t - 1.0)
-
-  cdef DTYPE_t out_dot = 0.0
-  for i in range(56):
-    out_dot += coeffs[i] * exp(exponents[i])
-  return out_dot
+  return out
